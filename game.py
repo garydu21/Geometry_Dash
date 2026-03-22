cb5b4723e2bfcb2968e506e9ad4d8c46ee6da699
Plastik0 <chris.kalouche2005@gmail.com>
2026-03-22 19:35:50 +0100

    Ajout :    Placement des blocs et des spikes    Deplacement de la camera

--- game.py
+++ game.py
@@ -16,6 +16,12 @@ pygame.display.set_caption("Hello Pygame")
 def lanceur():
     # Game loop
     running = True
+    camera_x = 0
+    speed = 1.5
+    map = read_csv(level)
+    skin = {
+        "0": pygame.image.load(f"images/block_texture/block_{level}.png").convert_alpha()
+            }
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -23,11 +29,12 @@ def lanceur():
                     running = False
             if event.type == pygame.QUIT:
                 running = False
-        for x in range(3):
+        for x in range(15):
             for y in range(2):
-                screen.blit(bg, (x*800, y*800))
+                screen.blit(bg, (x * 800 - camera_x, y * 800))
+        build_map(screen,camera_x,0,skin,map)
         pygame.display.update()
-
+        camera_x += speed
 
 
     # Quit Pygame
--- world_generation.py
+++ world_generation.py
@@ -1,4 +1,4 @@
-import csv
+import csv, pygame.draw
 
 def image_backgroung(level):
     return f"images/level1/bg_{level}.png"
@@ -12,7 +12,16 @@ def read_csv(level):
             matrice_niveau.append(row)
     return matrice_niveau
 
-def build_map(level):
-    path = f"maps/level_{level}.csv"
-    map = read_csv(path)
+def build_map(screen,camera_x,camera_y,skin,map):
+    hauteur = len(map)
+    longeur = len(map[0])
+    size = 80
+    bloc = skin["0"]
 
+    for y in range(hauteur):
+        for x in range(longeur):
+            element = map[y][x]
+            if element == "0":
+                screen.blit(bloc, (x*size - camera_x, y*size - 400))
+            if element == "S":
+                pygame.draw.rect(screen, (0,255,0),(x*size-camera_x,y*size-400,size,size))
