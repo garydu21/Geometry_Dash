ca686622e732ddf1a04a44040f8f6308cec30f53
Plastik0 <chris.kalouche2005@gmail.com>
2026-03-27 17:39:13 +0100

    Ajout :    Systeme de gravité sur la version console    Saut fonctionnel sur la version console    Detection lors de l'accomplissement d'un niveau    Detection lors d'une defaite

--- render_console.py
+++ render_console.py
@@ -1,9 +1,10 @@
 import time
 from pynput.keyboard import *
+from pynput.mouse import Listener as MouseListener, Button
 
 from world_generation import *
 
-level = 1
+level = 0
 
 taille_jeu = 16
 
@@ -18,31 +19,40 @@ map_matrice = [ligne[:taille_jeu] for ligne in map_complete]
 joueur_x = 0
 joueur_y = 0
 
+action_fini = True
 saute = False
-saut_restant = 0
+saut_restant = -1
 
-vitesse = 1
+perdu = False
+vitesse = 0.7
 
 
 def render_console(level):
-    global map_matrice, hauteur, longeur
+    global map_matrice, hauteur, longeur, perdu
 
     apparition_joueur()
 
     compteur,niveau_fini = avancement()
-
-    while not niveau_fini:
+    while not niveau_fini and not perdu:
         physique()
-        for x in range(hauteur):
-            for y in range(taille_jeu):
-                if x == joueur_x and y == joueur_y:
-                    print('b', end='  ')
-                else:
-                    print(map_matrice[x][y], end='  ')
-            print()
 
-        compteur, niveau_fini = avancement()
-        time.sleep(vitesse)
+        if collision() and compteur > 3:
+            perdu = True
+        if not niveau_fini:
+            for x in range(hauteur):
+                for y in range(taille_jeu):
+                    if x == joueur_x and y == joueur_y:
+                        print('b', end='  ')
+                    else:
+                        print(map_matrice[x][y], end='  ')
+                print()
+
+            compteur, niveau_fini = avancement()
+            time.sleep(vitesse)
+    if niveau_fini:
+        print("COMPLETED")
+    else:
+        print("GAME OVER")
 
 def apparition_joueur():
     global joueur_x, joueur_y
@@ -62,7 +72,7 @@ def apparition_joueur():
 def avancement():
     global joueur_x, joueur_y, compteur, map_matrice
 
-    niveau_fini = compteur == longeur - 1
+    niveau_fini = map_matrice[joueur_x][joueur_y] == "End"
 
     if compteur <= 3:
         joueur_y += 1
@@ -91,33 +101,55 @@ def au_sol():
     return False
 
 def physique():
-    global joueur_x, joueur_y, saute, saut_restant
+    global joueur_x, joueur_y, saute, saut_restant, action_fini
 
-    if not saute and au_sol():
+
+    if saute and au_sol():
+        saute = False
         saut_restant = 2
 
-    if saute:
-        if saut_restant == 2:
-            if joueur_x > 0 and map_matrice[joueur_x - 1][joueur_y] != '1':
-                joueur_x -= 1
-            saut_restant -= 1
-            print("ici")
-        elif saut_restant == 0:
+
+    if saut_restant == 2:
+        saut_restant -= 1
+        joueur_x -= 1
+    elif saut_restant == 0:
+        saut_restant = -1
+        action_fini = True
+    else:
+        saut_restant -= 1
+
+    if action_fini:
+        if map_matrice[joueur_x+1][joueur_y] != '0':
             joueur_x += 1
-            saute = False
-        else:
-            saut_restant -= 1
 
-
+def collision():
+    global joueur_x, joueur_y, gagner
+    colision = False
+    if map_matrice[joueur_x][joueur_y] == 'S':
+        colision = True
+    elif map_matrice[joueur_x][joueur_y] == '0':
+        colision = True
+    return colision
 
 
 def on_press(key):
-    global saute
-    if key == Key.space:
+    global saute, action_fini
+    if key == Key.space and not saute and action_fini:
         saute = True
+        action_fini = False
+
+def on_click(x, y, button, pressed):
+    global saute, action_fini
+    if button == Button.left and pressed and not saute and action_fini:
+        saute = True
+        action_fini = False
+
 
 listener = Listener(on_press=on_press)
 listener.start()
+
+mouse_listener = MouseListener(on_click=on_click)
+mouse_listener.start()
 
 render_console(level)
 
